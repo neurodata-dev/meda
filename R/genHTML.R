7b9ceaf92972d437d18c29fda89100157f19e420
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-01-04 15:38:03 -0500

    Updated 7 (pairs plot) [neurodata/meda#9] [neurodata/weekly-experiments#377]

--- R/genHTML.R
+++ R/genHTML.R
@@ -404,23 +404,44 @@ p.cumvar <- function(dat){
 }
 
 
-
-
-#' Generate a pairs plot
+#' Generate a pairs hex binned plot
 #'
 #' @param dat data
 #'
-#' @return A pairs plot
+#' @return A lattice splom
+#' @importFrom hexbin BTC
+#' @importFrom lattice splom
+#'
 #' @export 
 ### Pairs Plots
 p.pairs <- function(dat) {
 
   du <- ifelse(dim(dat)[2] > 8, 8, dim(dat)[2])
-  
-  t1 <-paste("pairs plot of first", du, "dimensions")
+  t1 <-paste("pairs hex binned plot of first", du, "dimensions")
 
-  pairs(as.matrix(dat)[, 1:du], pch = 19,  main = t1)
-}
+
+  tmp <- dat[, 1:du]
+
+  ### the following from https://procomun.wordpress.com/2011/03/18/splomr/
+  splom(tmp,
+   panel=panel.hexbinplot,
+   colramp=BTC,
+   diag.panel = function(x, ...){
+   yrng <- current.panel.limits()$ylim
+   d <- density(x, na.rm=TRUE)
+   d$y <- with(d, yrng[1] + 0.95 * diff(yrng) * y / max(y) )
+   panel.lines(d)
+   diag.panel.splom(x, ...)
+   },
+   lower.panel = function(x, y, ...){
+   panel.hexbinplot(x, y, ...)
+   panel.loess(x, y, ..., col = 'red')
+   panel.lmline(x, y, ..., col = 'orange')
+   },
+   pscale=0, varname.cex=0.7
+   )
+
+} ## END FUNCTION
 
 
 #' Generate a BIC plot
