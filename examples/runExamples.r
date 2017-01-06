5bfb48f6e8522f14f8e71e350d4946daef78b8e5
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-01-06 17:28:15 -0500

    Updated examples, jittered scatter plots. [neurodata/weekly-experiments#377]

--- examples/runExamples.r
+++ examples/runExamples.r
@@ -9,7 +9,7 @@ outfile3 <- "~/neurodata/GH-pages/meda-gh/examples/SynData01e3.html"
 
 #set.seed(2^13)
 set.seed(1234)
-dat <- data01[sample(nrow(data01), 1e3), 1:24, with = FALSE]
+dat <- data01[sample(nrow(data01), 500), 1:24, with = FALSE]
 
 #transformData <- function(x, type = c("1e3"), t98 = FALSE, ...) {
 datRaw <- dat
@@ -20,10 +20,8 @@ dat01e3 <- transformData(dat, type = c("1e3"))$d01e3
 genHTML(iris[,-5], outfile = outfile0, truth = iris[, 5])
 #system(paste("open", outfile0))
 genHTML(datRaw, outfile1)
-system(paste("open", outfile1))
+#system(paste("open", outfile1))
 genHTML(datLog, outfile2)
 #system(paste("open", outfile2))
 genHTML(dat01e3, outfile3)
 #system(paste("open", outfile3))
-genHTML(dat01e3, outfile3)
-
