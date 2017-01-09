ab744a2cf3c6600dd9256f1e2e3b7919622f72a8
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-01-09 16:07:05 -0500

    Added 3d pca on correlation matrix. [neurodata/meda#9] [neurodata/weekly-experiments#377]

--- examples/runExamples.r
+++ examples/runExamples.r
@@ -1,27 +1,68 @@
 #! /usr/local/bin/Rscript
 require(meda)
-load('~/neurodata/synaptome-stats/Code/cleanDataWithAttributes.RData')
+require(rhdf5)
 
 outfile0 <- "~/neurodata/GH-pages/meda-gh/examples/FisherIrisData.html"
-outfile1 <- "~/neurodata/GH-pages/meda-gh/examples/SynDataRaw.html"
-outfile2 <- "~/neurodata/GH-pages/meda-gh/examples/SynDataLog10.html"
-outfile3 <- "~/neurodata/GH-pages/meda-gh/examples/SynData01e3.html"
+genHTML(iris[,-5], outfile = outfile0, truth = iris[, 5])
+system(paste("open", outfile0))
 
-#set.seed(2^13)
+rm(list = ls())
+
+load('~/neurodata/synaptome-stats/Code/cleanDataWithAttributes.RData')
+
 set.seed(1234)
-dat <- data01[sample(nrow(data01), 500), 1:24, with = FALSE]
+dat <- data01[sample(nrow(data01), 1e3), 1:24, with = FALSE]
 
+ccol3 <- c("#197300", "#197300", "#197300", "#197300", "#197300", "#197300", 
+  "#197300", "#197300", "#197300", "#197300", "#197300", "#cc0000", 
+  "#cc0000", "#cc0000", "#cc0000", "#cc0000", "#cc0000", "#0000cd", 
+  "#0000cd", "#0000cd", "#0000cd", "#0000cd", "#0000cd", "#0000cd"
+  )
 #transformData <- function(x, type = c("1e3"), t98 = FALSE, ...) {
 datRaw <- dat
 datLog <- transformData(dat, type = c("log10"))$log10
 dat01e3 <- transformData(dat, type = c("1e3"))$d01e3
 
+outfile1 <- "~/neurodata/GH-pages/meda-gh/examples/SynDataRaw.html"
+outfile2 <- "~/neurodata/GH-pages/meda-gh/examples/SynDataLog10.html"
+outfile3 <- "~/neurodata/GH-pages/meda-gh/examples/SynData01e3.html"
 
-genHTML(iris[,-5], outfile = outfile0, truth = iris[, 5])
-#system(paste("open", outfile0))
-genHTML(datRaw, outfile1)
-#system(paste("open", outfile1))
-genHTML(datLog, outfile2)
-#system(paste("open", outfile2))
-genHTML(dat01e3, outfile3)
-#system(paste("open", outfile3))
+genHTML(datRaw, outfile1, colCol = ccol3)
+system(paste("open", outfile1))
+genHTML(datLog, outfile2, colCol = ccol3)
+system(paste("open", outfile2))
+genHTML(dat01e3, outfile3, colCol = ccol3)
+system(paste("open", outfile3))
+
+rm(list = ls())
+
+rhdf5::h5ls("~/neurodata/synaptome-stats/Code/Notebooks/Ex10R55_F0.h5")
+datH5 <- rhdf5::h5read("~/neurodata/synaptome-stats/Code/Notebooks/Ex10R55_F0.h5",
+                       name = "F0")
+datEX <- t(datH5[,,1])
+
+namesEx <- read.csv("~/neurodata/synaptome-stats/Code/Python/chan.csv", head=FALSE, stringsAsFactors = FALSE)[,1]
+
+set.seed(317)
+datRawEx <- as.data.frame(datEX[sample(nrow(datEX), 1e3), ])
+names(datRawEx) <- as.vector(namesEx)
+
+d <- c(3,26,1,25, 27:29)
+d <- c(d,setdiff(1:29, d))
+dat <- datRawEx[,d]
+
+datRaw <- dat
+datLog <- transformData(dat, type = c("log10"))$log10
+dat01e3 <- transformData(dat, type = c("1e3"))$d01e3
+
+outfile4 <- "~/neurodata/GH-pages/meda-gh/examples/Ex10R55_Raw.html"
+outfile5 <- "~/neurodata/GH-pages/meda-gh/examples/Ex10R55_Log10.html"
+outfile6 <- "~/neurodata/GH-pages/meda-gh/examples/Ex10R55_01e3.html"
+
+genHTML(datRaw, outfile4, colCol = NULL)
+system(paste("open", outfile4))
+genHTML(datLog, outfile5, colCol = NULL)
+system(paste("open", outfile5))
+genHTML(dat01e3, outfile6, colCol = NULL)
+system(paste("open", outfile6))
+
